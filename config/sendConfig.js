f712ff808f2e8c109d2510f81f3439a2ef38569c
sharad kumar <sharadkumar@sharads-MacBook-Air.local>
2026-05-27 01:10:06 +0530

    fixed gmail smtp config

--- config/sendConfig.js
+++ config/sendConfig.js
@@ -2,12 +2,14 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
