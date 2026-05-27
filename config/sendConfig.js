cf916462f37130682b62361eb839df3dd621edc4
sharad kumar <sharadkumar@sharads-MacBook-Air.local>
2026-05-28 03:10:20 +0530

    opt sectio fix

--- config/sendConfig.js
+++ config/sendConfig.js
@@ -1,22 +1,19 @@
-import nodemailer from "nodemailer";
+import { TransactionalEmailsApi, SendSmtpEmail } from "@getbrevo/brevo";
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  const apiInstance = new TransactionalEmailsApi();
+  apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
 
-  await transporter.sendMail({
-    from: `"MasterHire" <123sharadkumar6@gmail.com>`,
-    to,
-    subject,
-    text,
-  });
+  const sendSmtpEmail = new SendSmtpEmail();
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.textContent = text;
+  sendSmtpEmail.sender = {
+    name: "MasterHire",
+    email: "123sharadkumar6@gmail.com",
+  };
+  sendSmtpEmail.to = [{ email: to }];
+
+  await apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 
 export default sendEmail;
