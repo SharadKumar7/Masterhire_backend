6c49a641a6e0a836b888876bc9ddc172e97e82f7
sharad kumar <sharadkumar@sharads-MacBook-Air.local>
2026-05-28 00:58:20 +0530

    fix: port no.

--- config/sendConfig.js
+++ config/sendConfig.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 465,
     secure: false,
     auth: {
       user: process.env.EMAIL_USER,
